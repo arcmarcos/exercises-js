6fecebf0e3b8a0b4e23711fe76f0ac3d3f588351
Marcos Antonio <marcos-rj@outlook.com>
2021-03-29 16:44:46 -0300

    few adds, readme and demonstration

---
+++ vagalume-api/README.md
@@ -0,0 +1,8 @@
+# Aplicação de consumo da API do Vagalume
+
+[API Vagalume](https://api.vagalume.com.br/)
+
+*Função do botão de  tradução ainda não implementada.*
+
+### Demonstração ↓
+![Demonstração em webp](./demonstracao.webp)
--- vagalume-api/main.js
+++ vagalume-api/main.js
@@ -1,12 +1,10 @@
 const form = document.forms[0];
-const alert = document.querySelectorAll('.alert')
-console.log(alert[0]);
-
-form.addEventListener("submit", handleSubmit)
+const alert = document.querySelectorAll('.alert');
+const lyricDiv = document.querySelector(".lyric-result");
 
 function alertUser(element) {
   alert[element].classList.add('active');
-  setTimeout(() => alert[element].classList.remove('active'), 3000);
+  setTimeout(() => alert[element].classList.remove('active'), 2000);
 }
 
 function handleSubmit(e) {
@@ -21,43 +19,66 @@ function handleSubmit(e) {
   }
 }
 
+form.addEventListener("submit", handleSubmit);
+
 function fillDiv(picture, artist, song, lyric) {
   const resultElements = `
      <img src=${picture} alt=${artist} title=${artist}>
-     <h2>${artist} | ${song}</h2>
-     <pre>${lyric}</pre>`;
+     <h2>${artist} | ${song}</h2>`;
 
-  const lyricDiv = document.querySelector(".lyric-result");
-  lyricDiv.innerHTML = resultElements;
+  const lyricElement = `<pre>${lyric}</pre>`;
+
+  lyricDiv.innerHTML = resultElements + lyricElement;
+}
+
+function fetchedSong({ art, mus }) {
+  let picture = art.pic_medium;
+  let artistName = art.name;
+  let songName = mus[0].name;
+  let lyric = mus[0].text;
+  let lyricTranslate =  mus[0].translate ? mus[0].translate[0].text : null
+
+  fillDiv(picture, artistName, songName, lyric);
+
+  lyricTranslate ? translateSong() : null;
+
+  document.title = `${songName} | ${artistName}`;
+  document.querySelector('link[rel="icon"]').href = `${picture}`;
+}
+
+function notFetchedSong() {
+  const notFound = document.createElement('p');
+  notFound.classList.add('not-found');
+  notFound.innerText = 'Música ou artista não encontrado!';
+  form.append(notFound);
+  lyricDiv.innerHTML = '';
+  setTimeout(() => notFound.remove(), 3000)
+
+  if (notFound.previousSibling.nodeName === 'P') {
+    notFound.remove(); 
+  }
+}
+
+function translateSong() {
+  const translateButton = document.createElement('button');
+  translateButton.classList.add('translate');
+  translateButton.innerHTML = 'Tradução';
+  if (translateButton) {
+    lyricDiv.insertAdjacentElement('afterbegin', translateButton);
+  }
 }
 
 async function fetchLyric(song, artist) {
-  const url = `https://api.vagalume.com.br/search.php?art=${artist}&extra=artpic&mus=${song}`;
+  const url = `
+  https://api.vagalume.com.br/search.php?art=${artist}&extra=artpic&mus=${song}`;
   const response = await fetch(url);
   const result = await response.json();
 
-  let picture, artistName, songName, lyric;
-
   if (result.type === 'exact') {
-    picture = result.art.pic_medium;
-    artistName = result.art.name;
-    songName = result.mus[0].name;
-    lyric = result.mus[0].text;
-
-    fillDiv(picture, artistName, songName, lyric);
-    document.title = `${songName} | ${artistName}`;
-    document.querySelector('link[rel="icon"]').href = `${picture}`
-  }
-  else {
-        const notFound = document.createElement('p');
-        notFound.classList.add('not-found')
-        notFound.innerText = "Música ou artista não encontrado!"
-        form.append(notFound)
-                
-        setTimeout(() => notFound.remove(), 3000)
-
-        if(notFound.previousSibling.nodeName === 'P') {
-          notFound.remove(); 
-        }
+    fetchedSong(result);
+    form.song.value = '';
+    form.artist.value = '';
+  } else {
+    notFetchedSong();
   }
 }
